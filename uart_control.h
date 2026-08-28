a7a0a4406e44764f0cf6adcc1ac3f552bdaa857e
eryastikayasa <eryastikayasa@gmail.com>
2026-08-29 07:12:03 +0800

    Add files via upload

---
+++ uart_control.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void uart_control_init(void);
+void uart_control_send(const char *cmd);
+int  uart_control_read(char *buf, size_t max_len); // return jumlah byte terbaca, -1 jika tidak ada data
+
+#ifdef __cplusplus
+}
+#endif
