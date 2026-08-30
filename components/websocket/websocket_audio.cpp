bbd0be0608bd630d9c11754e0d8448029be8c231
eryastikayasa <eryastikayasa@gmail.com>
2026-08-30 22:20:28 +0800

    Change audio playback task to run on core 1
    Changed the core affinity of the audio playback task from core 0 to core 1.

--- components/websocket/websocket_audio.cpp
+++ components/websocket/websocket_audio.cpp
@@ -252,7 +252,7 @@ bool start_audio_playback(void)
 
     BaseType_t result = xTaskCreatePinnedToCore(audio_playback_task, "audio_playback",
                                                 4096, NULL, 4,
-                                                &audio_playback_task_handle, 0);
+                                                &audio_playback_task_handle, 1);
     if (result != pdPASS) {
         ESP_LOGE(TAG, "Gagal membuat audio_task/playback task: free_internal=%u largest=%u",
                  (unsigned)heap_caps_get_free_size(MALLOC_CAP_INTERNAL),
