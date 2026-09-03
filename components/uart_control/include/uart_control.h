d201a402befa6d7e2838b349d7c052551a64c6ef
eryastikayasa <eryastikayasa@gmail.com>
2026-09-03 12:36:58 +0800

    Fix UART tool command declaration

--- components/uart_control/include/uart_control.h
+++ components/uart_control/include/uart_control.h
@@ -10,6 +10,7 @@ extern "C" {
 void uart_control_init(void);
 void uart_control_send(const char *cmd);
 int  uart_control_read(char *buf, size_t max_len); // return jumlah byte terbaca, -1 jika tidak ada data
+bool uart_control_execute_command(const char *cmd);
 
 #ifdef __cplusplus
 }
