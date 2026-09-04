99fa444221be7bec5f4edd7cff0fa4f8e93370fd
eryastikayasa <eryastikayasa@gmail.com>
2026-09-05 07:55:47 +0800

    Revert WebConfig POST buffer to internal RAM for DNS isolation test

--- components/web_config/web_config.cpp
+++ components/web_config/web_config.cpp
@@ -10,7 +10,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "esp_attr.h"
 
 static const char *TAG = "WEB_CONFIG";
 
@@ -141,7 +140,7 @@ static esp_err_t root_get_handler(httpd_req_t *req)
 
 static esp_err_t save_post_handler(httpd_req_t *req)
 {
-    static EXT_RAM_BSS_ATTR char content[2048];
+    char content[2048];
     int total = 0;
     while (total < (int)sizeof(content) - 1) {
         int received = httpd_req_recv(req, content + total, sizeof(content) - 1 - total);
